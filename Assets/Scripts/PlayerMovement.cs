6be954810a99cbb807b0a74fc483f7956b84a796
Marcel van Duijn <epicmars264@gmail.com>
2022-02-04 04:15:18 +0100

    V0.1.0
    Changes: 
    
    * Added Material Mat_Gate
    * Added AudioFile Retro_Action_GameTheme_3.wav
    * Added AudioFile Retro_Action_GameTheme_5.wav
    * Added AudioFile Retro_Action_GameTheme_8.wav
    * Added TestMap LightBake
    * Added TestMap_BossFight.unity
    * Added TestMap_BossFight LightBake.unity
    * Added DoEvent.cs
    * Added OnCollision.cs
    
    * Updated Player.prefab
    * Updated Endless.unity
    * Updated Menu.unity
    * Updated PlayerMovement.cs
    * Updated ScoreHandler.cs
    
    * Removed Madness (Looped).wav
    * Removed Campfire.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,52 +8,55 @@ public class PlayerMovement : MonoBehaviour {
     [Header("Refrences")]
     [SerializeField] private Camera _Camera;
     [SerializeField] private GameObject _PlayerObject;
-    [Header("Speed")]
-    [SerializeField] private float _NormalSpeed;
-    [SerializeField] private float _SprintSpeed, _DashSpeed, _JumpSpeed;
-    [Header("Other Settings")]
-    [SerializeField] private float _Gravity;
-    [SerializeField] private float _DashDistance;
 
-    private bool _Dashing;
-    private Vector3 _DashPosition;
-    private float _Speed;
+    [Header("Speed")]
+    [SerializeField] private float _NormalSpeed = 10;
+    [SerializeField] private float _SprintSpeed = 15;
+
+    [Header("Gravity")]
+    [SerializeField] private float _Gravity = 3;
+
+    [Header("Roll Dash")]
+    [SerializeField] private float _RollDuration = 2;
+    [SerializeField] private float _DashSpeed = 20;
+
+    [Header("Dash Visual")]
+    [SerializeField] private GameObject _PlayerObj_Cube = null;
+    [SerializeField] private GameObject _PlayerObj_Sphere = null;
+
+    private Vector3 _DashDirection = Vector3.zero;
     private Vector3 moveDirection = Vector3.zero;
-    private BoxCollider _BC;
     private CharacterController _CC;
+    private bool _Dashing;
+    private float _Speed;
 
     void Start()
     {
-        _BC = GetComponent<BoxCollider>();
         _CC = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         //Movement
-        if (_CC.isGrounded)
+        if (!_Dashing)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= _Speed;
-            //if (Input.GetButton("Jump"))
-            //    moveDirection.y = _JumpSpeed;
-        }
-        moveDirection.y -= _Gravity * Time.deltaTime;
-        _CC.Move(moveDirection * Time.deltaTime);
-
-        //Dash
-        if(Input.GetButtonDown("Jump"))
-            Dash();
-        if(_Dashing)
-        {
-            transform.position = Vector3.MoveTowards(transform.position,_DashPosition, _DashSpeed * Time.deltaTime);
-            if(transform.position == _DashPosition)
+            if (_CC.isGrounded)
             {
-                _BC.enabled = true;
-                _Dashing = false;
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= _Speed;
             }
+            moveDirection.y -= _Gravity * Time.deltaTime;
+            _CC.Move(moveDirection * Time.deltaTime);
+
+            //Dash
+            if (Input.GetButtonDown("Jump"))
+                StartCoroutine(IDash());
         }
+        else
+        {
+            _CC.Move(_DashDirection * _DashSpeed * Time.deltaTime);
+        } 
 
         //Sprint
         if (Input.GetKey(KeyCode.LeftShift))
@@ -61,34 +64,43 @@ public class PlayerMovement : MonoBehaviour {
         else
             _Speed = _NormalSpeed;
 
+        //Look At Mouse Position
         Ray cameraRay = _Camera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayLength;
         if (groundPlane.Raycast(cameraRay, out rayLength))
         {
             Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-
             _PlayerObject.transform.LookAt(new Vector3(pointToLook.x, _PlayerObject.transform.position.y, pointToLook.z));
+        }
+
+        //Visual
+        if(_Dashing)
+        {
+            _PlayerObj_Cube.SetActive(false);
+            _PlayerObj_Sphere.SetActive(true);
+        }    
+        else
+        {
+            _PlayerObj_Cube.SetActive(true);
+            _PlayerObj_Sphere.SetActive(false);
         }
     }
 
-    private void Dash()
+    private IEnumerator IDash()
     {
-        RaycastHit hit;
+        //Dash Visual On
+        _PlayerObj_Cube.SetActive(false);
+        _PlayerObj_Sphere.SetActive(true);
 
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal") * _DashDistance,0, Input.GetAxis("Vertical") * _DashDistance);
-        Vector3 destination = _PlayerObject.transform.position + direction;
+        //Apply Dash
+        _DashDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _Dashing = true;
+        yield return new WaitForSeconds(_RollDuration);
+        _Dashing = false;
 
-        if (!Physics.Linecast(_PlayerObject.transform.position, destination, out hit))
-        {
-            _Dashing = true;
-            _BC.enabled = false;
-            if (Physics.Raycast(destination, -Vector3.up, out hit))
-            {
-                destination = hit.point;
-                destination.y = transform.position.y;
-                _DashPosition = destination;
-            }
-        }
+        //Dash Visual Off
+        _PlayerObj_Cube.SetActive(true);
+        _PlayerObj_Sphere.SetActive(false);
     }
 }
--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -9,15 +9,22 @@ public class ScoreHandler : MonoBehaviour
     [Header("Score")]
     [SerializeField] private TextMeshProUGUI _ScoreText;
 
+    [Header("Settings")]
+    [SerializeField] private float _SizeIncrease;
+    [SerializeField] private float _HighestScoreAmount;
+    [SerializeField] private float _SizeDecreaseSpeed;
+
     [Header("Time")]
     [SerializeField] private TextMeshProUGUI _AliveTimeText;
 
     private float _Score;
     private float _AliveTime;
     private float _ScoreEncreaseTimer;
+    private float _LocalScale;
 
     void Start()
     {
+        _LocalScale = _ScoreText.fontSize;
         _ScoreText.text = "0";
         _AliveTimeText.text = "0:00:00";
     }
@@ -30,18 +37,27 @@ public class ScoreHandler : MonoBehaviour
             AddScore(1000);
             _ScoreEncreaseTimer = 0;
         }
-
         _AliveTime += 1 * Time.deltaTime;
 
+        //To Text
         _ScoreText.text = _Score.ToString("0");
         _AliveTimeText.text = string.Format("{0:00}:{1:00}:{2:00}", Mathf.Floor(_AliveTime / 3600), Mathf.Floor((_AliveTime / 60) % 60), _AliveTime % 60);
+
+        //Score Text Increase
+        if (_ScoreText.fontSize > _LocalScale)
+            _ScoreText.fontSize -= _SizeDecreaseSpeed * Time.deltaTime;
+        else
+            _ScoreText.fontSize = _LocalScale;
     }
 
     public void AddScore(float scoreAdd)
     {
         _Score += scoreAdd;
-
-
-
+        float sizeincrease = 100 + (scoreAdd / _HighestScoreAmount) * _SizeIncrease;
+        if (sizeincrease > 50)
+            sizeincrease = 50;
+        if (_ScoreText.fontSize <= sizeincrease)
+            _ScoreText.fontSize = sizeincrease;
+        _ScoreText.text = _Score.ToString("0");
     }
 }
